4d185d2dee9a7467fe3399556ed1e2cc36d918eb
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-03-27 17:17:58 -0700

    version 2/0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.soupslurpr.transcribro"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
