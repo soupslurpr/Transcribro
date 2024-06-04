358f695e0234dab82144aabee5e93e0f01e12c5b
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-06-03 22:07:51 -0700

    Ensure audio inputs are a certain length to prevent them going unprocessed

--- app/src/main/kotlin/dev/soupslurpr/transcribro/recognitionservice/whisper/WhisperRepository.kt
+++ app/src/main/kotlin/dev/soupslurpr/transcribro/recognitionservice/whisper/WhisperRepository.kt
@@ -20,9 +20,22 @@ class WhisperRepository(
     suspend fun transcribeAudio(data: ShortArray): String {
         loadWhisperContextIfNull()
         // assume we only have one channel
-        val buffer = FloatArray(data.size) { index ->
+        var buffer = FloatArray(data.size) { index ->
             (data[index] / 32767.0f).coerceIn(-1f..1f)
         }
+
+        if (data.size < 32000) {
+            val newBuffer = FloatArray(32000)
+
+            for ((i, value) in buffer.withIndex()) {
+                newBuffer[i] = value
+            }
+
+            newBuffer.fill(0f, data.size, newBuffer.size)
+
+            buffer = newBuffer
+        }
+
         val transcript = whisperContext.value?.transcribeData(buffer, ((data.size / 16000f) * 1000f).toLong()) ?: ""
         return transcript.removeSuffix(" .") // remove hallucination
     }
