30a2814f1f8492f5f79f79ac911af979aa214b10
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-06-23 18:49:14 -0700

    update androidx.lifecycle:lifecycle-runtime-ktx to 2.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,10 +90,9 @@ dependencies {
     implementation(project(":lib"))
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-ktx:1.9.0")
 
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
