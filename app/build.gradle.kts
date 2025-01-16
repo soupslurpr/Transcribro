c58e8d22448ce1ba9b3251ad93f3b45bc824f8be
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-01-16 15:45:49 -0800

    increase version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.soupslurpr.transcribro"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.0"
+        versionCode = 6
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
