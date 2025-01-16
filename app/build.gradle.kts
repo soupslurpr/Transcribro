7bf00ebc5d5e0561e178e976cf7110897762c690
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-01-16 13:27:02 -0800

    update AndroidX Core to 1.15.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
 
     implementation(project(":lib"))
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.navigation:navigation-compose:2.8.2")
