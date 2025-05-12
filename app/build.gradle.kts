d4bbdfddd36c9ceea08d234e0376f3dfe50025f4
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-05-11 21:47:44 -0700

    update AndroidX Activity Compose to 1.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
-    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("androidx.activity:activity-ktx:1.10.0")
 
