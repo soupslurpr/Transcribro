a91f0462b0696304f434748a445fd13149c7b439
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-05-11 22:18:40 -0700

    update AndroidX DataStore Preferences to 1.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    implementation("androidx.datastore:datastore-preferences:1.1.2")
+    implementation("androidx.datastore:datastore-preferences:1.1.6")
     implementation("com.google.accompanist:accompanist-permissions:0.36.0")
     implementation("com.microsoft.onnxruntime:onnxruntime-android:1.20.0")
 }
