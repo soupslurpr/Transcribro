804593216d02021f53eda305d9485c0280986801
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-05-11 21:40:15 -0700

    update Kotlin to 2.1.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
 }
 
 android {
