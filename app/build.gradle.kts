e4a0f737ce0350c06175d6f8a62d5e5995d319cc
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-05-31 20:33:38 -0700

    update Kotlin to 2.1.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21"
 }
 
 android {
