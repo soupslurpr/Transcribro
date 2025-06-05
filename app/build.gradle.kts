b9399d0663898de149606dd760f5499eca66b271
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-06-04 18:22:10 -0700

    update AndroidX Lifecycle libraries to 2.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     implementation(project(":lib"))
 
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
