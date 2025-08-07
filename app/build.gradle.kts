33f7812617692656aa5843f7c5ffea2c0b5b7bc0
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-08-06 18:37:59 -0700

    update AndroidX Navigation Compose to 2.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.navigation:navigation-compose:2.9.1")
+    implementation("androidx.navigation:navigation-compose:2.9.3")
     implementation("androidx.activity:activity-ktx:1.10.1")
 
     implementation(platform("androidx.compose:compose-bom:2025.06.01"))
